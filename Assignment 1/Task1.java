40b8a6eb8d0f84b85d0985372838bc48114a6f4f
Asadullah Al Imran <66588286+asadullah00alimran@users.noreply.github.com>
2020-07-06 22:45:37 +0600

    Add files via upload

---
+++ Assignment 1/Task1.java
@@ -0,0 +1,19 @@
+package assignment1;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+public static void main(String[] args){
+    
+    Scanner input = new Scanner(System.in);
+    
+    int number;
+    
+    System.out.print("Enter a number : ");
+    number = input.nextInt();
+    
+    System.out.println("The number is : "+number);
+    
+}
+}
