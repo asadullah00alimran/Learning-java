21ed6dabca8b6b66fd96ab26d967f27a1e714078
Asadullah Al Imran <66588286+asadullah00alimran@users.noreply.github.com>
2020-07-08 23:25:08 +0600

    Add files via upload

---
+++ Assignment 1/Task9.java
@@ -0,0 +1,14 @@
+package assignment1;
+
+public class Task9 {
+
+    public static void main(String[] args) {
+        
+        int i;
+        for(i=0;i<=9;i++)
+        {
+            System.out.println(i);
+        }
+    }
+    
+}
