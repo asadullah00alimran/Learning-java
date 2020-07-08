61bccab157dc56881605592637be619d93a50148
Asadullah Al Imran <66588286+asadullah00alimran@users.noreply.github.com>
2020-07-09 00:03:24 +0600

    Add files via upload

---
+++ Assignment 1/Task10.java
@@ -0,0 +1,17 @@
+package assignment1;
+
+public class Task10 {
+    
+    public static void main(String[] args) {
+    
+        int i;
+        
+        for(i=0;i<20;i++)
+        {
+            System.out.println(i);
+            
+            i = i + 1;
+        }
+    }
+    
+}
