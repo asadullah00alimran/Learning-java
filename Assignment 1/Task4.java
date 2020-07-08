9a5a0d4ea544a41a86b9c4ef221bb18e34c75b67
Asadullah Al Imran <66588286+asadullah00alimran@users.noreply.github.com>
2020-07-08 23:09:22 +0600

    Add files via upload

---
+++ Assignment 1/Task4.java
@@ -0,0 +1,26 @@
+package assignment1;
+
+import java.util.Scanner;
+
+
+public class Task4 {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        int num1,num2;
+        
+        System.out.print("Enter two numbers : ");
+        
+        num1 = input.nextInt();
+        num2 = input.nextInt();
+        
+        if(num1>num2)
+        {
+            System.out.println("first");
+        }
+       
+                
+    }
+    
+}
