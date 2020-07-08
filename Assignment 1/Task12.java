28bfaf029783423667c649700a305c2a38d8edd8
Asadullah Al Imran <66588286+asadullah00alimran@users.noreply.github.com>
2020-07-09 00:06:06 +0600

    Add files via upload

---
+++ Assignment 1/Task12.java
@@ -0,0 +1,18 @@
+package assignment1;
+
+
+
+public class Task12 {
+ 
+    public static void main(String[] args) {
+        
+        int i,sum=0;
+        
+        for(i=1;i<=100;i++)
+        {
+            sum = sum +i;
+            
+    }
+        System.out.println(sum);
+}
+}
