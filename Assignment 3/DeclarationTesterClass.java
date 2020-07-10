92ca552f693c92df6cdc9969808392abfbbff3e5
Asadullah Al Imran <66588286+asadullah00alimran@users.noreply.github.com>
2020-07-10 23:11:55 +0600

    Add files via upload

---
+++ Assignment 3/DeclarationTesterClass.java
@@ -0,0 +1,14 @@
+
+package assignment3;
+
+public class DeclarationTesterClass {
+    
+    public static void main(String[] args) {
+        
+        DeclarClass dc = new DeclarClass();
+        
+        dc.inputData();
+        dc.printData();
+    }
+    
+}
